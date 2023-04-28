9036cf298c5450fbac157ae8209ae23232e5a285
Kozoloskit <kozoloski@yahoo.com>
2023-04-28 21:15:39 +0200

    Homework2

---
+++ Homework2/Homework2/Homework2/Program.cs
@@ -0,0 +1,58 @@
+﻿string appPath = @"..\..\..\Exercise";
+string filePath = appPath + @"\calculations.txt";
+
+void CreateFolder(string path)
+{
+    if (!Directory.Exists(path))
+    {
+        Directory.CreateDirectory(path);
+    }
+}
+
+void CreateFile(string path)
+{
+    if (!File.Exists(path))
+    {
+        File.Create(path).Close();
+    }
+}
+
+string Calculate(int num1, int num2)
+{
+    int result = num1 + num2;
+    return num1 + " + " + num2 + " = " + result;
+}
+
+
+
+void Calculation()
+{
+    try
+    {
+        Console.Write("Enter number one: ");
+        int num1 = int.Parse(Console.ReadLine());
+        Console.Write("Enter number two: ");
+        int num2 = int.Parse(Console.ReadLine());
+
+    
+        string result = Calculate(num1, num2);
+        Console.WriteLine(result);
+
+
+        using (StreamWriter sw = File.AppendText(filePath))
+        {
+            sw.WriteLine(result + " " + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"));
+        }
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.Message);
+    }
+
+
+}
+
+Calculation();
+CreateFolder(appPath);
+CreateFile(filePath);
+
